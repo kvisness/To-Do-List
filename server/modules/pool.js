d1a4a747b10fbbd8517fe96a0195c6c0cf0b4475
Kris Visness <kvisness@hbm2.com>
2020-09-29 08:56:45 -0500

    set up database correctly

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,22 +1,56 @@
+/**
+ * You'll need to use environment variables in order to deploy your
+ * pg-pool configuration to Heroku.
+ * It will look something like this:
+ **/
+/* the only line you likely need to change is
+ database: 'prime_app',
+ change `prime_app` to the name of your database, and you should be all set!
+*/
+
 const pg = require("pg");
-const Pool = pg.Pool;
-const config = {
-  database: "weekend-to-do-app", // the name of the database
-  host: "localhost", // where is your database
-  port: 5432, // the port number for your database, 5432 is the default
-  max: 10, // how many connections at one time
-  idleTimeoutMillis: 30000, // 30 seconds to try to connect
-};
+const url = require("url");
 
-// one instance to rul them all!
-const pool = new Pool(config);
+let config = {};
 
-pool.on("connect", (client) => {
-  console.log("pg connected");
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(":");
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split("/")[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
+  config = {
+    host: "localhost", // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: "weekend-to-do-app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+// the pool will log when it connects to the database
+pool.on("connect", () => {
+  console.log("Postgesql connected");
 });
 
-pool.on("error", (err, client) => {
-  console.log("Unexpected error on idle pg client", err);
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on("error", (err) => {
+  console.log("Unexpected error on idle client", err);
   process.exit(-1);
 });
 
